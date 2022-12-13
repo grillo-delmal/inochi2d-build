900dd1093d2dc2a0499a162fb733fa51ff4be3fd
Grillo del Mal <grillo@delmal.cl>
2022-12-13 18:57:50 -0300

    Cleanup Macros

--- scripts/spec_gen.py
+++ scripts/spec_gen.py
@@ -3,10 +3,19 @@ import math
 def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
 
     with open(path, 'w') as f:
+
+        # Macros
+
         f.write('\n'.join([
             "%global debug_package %{nil}",
-            "%define __strip /bin/true",
-            "%define __brp_mangle_shebangs /bin/true",
+            ""
+        ]))
+        if "macros" in data and len(data["macros"]) > 0:
+            f.write('\n'.join(data["macros"]))
+
+        # Variables
+
+        f.write('\n'.join([
             "",
             "%%define lib_name      %s" % name,
             "%%define lib_ver       %s" % gitver.split("-")[0],
@@ -26,13 +35,18 @@ def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
                         data["vars"][key]) ,
                     ""
                 ]))
-
         f.write('\n'.join([
             "",
             "%if 0%{lib_dist} > 0",
             "%define lib_suffix ^%{lib_dist}.git%{lib_short}",
             "%endif",
             "",
+            ""
+        ]))
+
+        # General description
+
+        f.write('\n'.join([
             "Name:           zdub-%{lib_name}",
             "Version:        %{lib_ver}%{?lib_suffix:}",
             "Release:        %autorelease",
@@ -42,7 +56,9 @@ def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
             "URL:            %s" % (data["url"] if "url" in data else "https://github.com/Inochi2D/%{lib_name}"),
             ""
         ]))
-        ## SOURCES AND PATCHES
+
+        # Sources and patches
+
         if "sources" in data and len(data["sources"]) > 0:
             for i in range(len(data["sources"])):
                 f.write('\n'.join([
@@ -87,23 +103,27 @@ def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
                         data["patches"][i]) ,
                     ""
                 ]))
+        f.write('\n')
+
+        # Build Requirements
 
         f.write('\n'.join([
-            "",
             "BuildRequires:  setgittag",
             "BuildRequires:  git",
             ""
         ]))
-        ## BUILD REQUS
         if "build_reqs" in data and len(data["build_reqs"]) > 0:
             for build_req in data["build_reqs"]:
                 f.write('\n'.join([
                     "BuildRequires:  %s" % build_req ,
                     ""
                 ]))
+        f.write('\n')
+        f.write('\n')
+
+        # Description
+
         f.write('\n'.join([
-            "",
-            "",
             "%description",
             ""
         ]))
@@ -115,9 +135,12 @@ def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
                 "#FIXME: generate an actual description",
                 ""
             ]))
+        f.write('\n')
+        f.write('\n')
+
+        # Devel package info and requirements
+
         f.write('\n'.join([
-            "",
-            "",
             "%package devel",
             "Provides:       %{name}-static = %{version}-%{release}",
             "Summary:        Support to use %{lib_name} for developing D applications",
@@ -142,8 +165,14 @@ def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
             "%description devel",
             "Sources to use the %{lib_name} library on dub using the",
             "zdub-dub-settings-hack method.",
-            "",
-            "",
+            ""
+        ]))
+        f.write('\n')
+        f.write('\n')
+
+        # Preparation
+
+        f.write('\n'.join([
             "%prep",
             ""
         ]))
@@ -184,12 +213,22 @@ def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
         if "prep" in data and len(data["prep"]) > 0:
             f.write("\n")
             f.write('\n'.join(data["prep"]))
+        f.write('\n')
+        f.write('\n')
+
+        # Build data
+        # TODO: Prebuild binaries in the future?
+
         f.write('\n'.join([
-            "",
-            "",
             "%build",
             "",
             "",
+            ""
+        ]))
+
+        # Install data
+
+        f.write('\n'.join([
             "%install",
             "mkdir -p %{buildroot}%{_includedir}/zdub/%{lib_name}-%{lib_gitver}",
             "cp -r "
@@ -199,9 +238,12 @@ def spec_gen(path, name, gitver, semver, dist, commit, deps, data={}):
         ]))
         if "install" in data and len(data["install"]) > 0:
             f.write('\n'.join(data["install"]))
+        f.write('\n')
+        f.write('\n')
+
+        # File list
+
         f.write('\n'.join([
-            "",
-            "",
             "%files devel",
             "%license LICENSE",
             "%{_includedir}/zdub/%{lib_name}-%{lib_gitver}/%{lib_name}/",
