ecafae3ec783116be1ecd7f163f584a607473a32
Grillo del Mal <grillo@delmal.cl>
2024-02-28 11:25:52 -0300

    Remove debug mode from runners

--- run-creator.sh
+++ run-creator.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-if [[ -z ${DEBUG} ]]; then
-    LD_LIBRARY_PATH=$(pwd)/build_out/inochi-creator:${LD_LIBRARY_PATH} \
-        ./build_out/inochi-creator/inochi-creator "$@"
-else
-    LD_LIBRARY_PATH=$(pwd)/build_out/inochi-creator:${LD_LIBRARY_PATH} \
-        gdb \
-        --ex 'handle SIGUSR1 noprint nostop' \
-        --ex 'handle SIGUSR2 noprint nostop' \
-        --ex 'r' \
-        --args \
-        ./build_out/inochi-creator/inochi-creator "$@"
-fi
+LD_LIBRARY_PATH=$(pwd)/build_out/inochi-creator:${LD_LIBRARY_PATH} \
+    ./build_out/inochi-creator/inochi-creator "$@"
